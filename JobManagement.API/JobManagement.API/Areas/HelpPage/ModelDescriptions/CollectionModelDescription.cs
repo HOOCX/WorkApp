4ff8872123f54ebb4eeaad32159874009e416a49
Victoria Ramirez Charles <38332341+VictoriaRamirezCharles@users.noreply.github.com>
2021-08-13 11:17:36 -0400

    08132021

--- JobManagement.API/JobManagement.API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace JobManagement.API.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- JobManagement.API/JobManagement.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace JobManagement.API.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
